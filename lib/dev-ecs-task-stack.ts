b6c0e0fec38bb39c8be413eb0ca4a2203701632a
ennyu21986 <ennyu21986@cs.saisoncard.co.jp>
2024-12-05 15:50:39 +0900

    ECStaskでnginx:1.19.2-alpineを動かすところまで

--- lib/dev-ecs-task-stack.ts
+++ lib/dev-ecs-task-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+
 
 export class DevEcsTaskStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -23,6 +25,41 @@ export class DevEcsTaskStack extends cdk.Stack {
       desiredCapacity: 4
     });
 
+    // タスク定義作成
+    const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef');
+
+    // タスク定義設定
+    taskDefinition.addContainer('DefaultContainer', {
+      image: ecs.ContainerImage.fromRegistry("nginx:1.19.2-alpine"),
+      memoryLimitMiB: 256,
+      cpu: 128
+    }).addPortMappings({
+      containerPort: 80
+    });
+    // ECS Service作成
+    const ecsService = new ecs.Ec2Service(this, 'Service', {
+      cluster,
+      taskDefinition
+    });
+     // LoadBalancer作成
+     const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'LoadBalancer', {
+      vpc,
+      internetFacing: true
+    });
+
+    // TargetGroup作成
+    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'TargetGroup', {
+      vpc,
+      port: 80
+    });
+
+    // LoadBanacerと紐付け
+    loadBalancer.addListener('Listener', {
+      port: 80,
+      defaultTargetGroups: [targetGroup]
+    })
+    // TargetGroupとECS Serviceを紐付け
+    targetGroup.addTarget(ecsService);
 
   }
 }
