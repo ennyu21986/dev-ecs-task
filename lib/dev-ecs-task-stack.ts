be3e78dcd3755c2375fefdc4cb5aea7edec3550f
ennyu21986 <ennyu21986@cs.saisoncard.co.jp>
2024-12-05 15:22:40 +0900

    vpc,ecs culsterを追加

--- lib/dev-ecs-task-stack.ts
+++ lib/dev-ecs-task-stack.ts
@@ -1,10 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 
 export class DevEcsTaskStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // VPC作成
+    const vpc = new ec2.Vpc(this, 'ecs-vpc', {
+      cidr: "10.210.0.0/16"
+    });
+    // ECS Cluster作成
+    const cluster = new ecs.Cluster(this, 'cluster', {
+      vpc
+    });
+
+    // ClusterにAutoScalingGroupなt3.microを追加
+    cluster.addCapacity('DefaultAutoScalingGroupCapacity', {
+      instanceType: new ec2.InstanceType("t3.micro"),
+      desiredCapacity: 4
+    });
+
 
   }
 }
